52ad8c5d7b5c1e5717f575049fd86dd55be7dae6
SamsonChoo <hanye_choo@mymail.sutd.edu.sg>
2020-12-06 19:28:46 +0800

    crud file draft

---
+++ crud.py
@@ -0,0 +1,32 @@
+import requests
+import pymysql
+from dotenv import load_dotenv
+import os
+from json import dumps
+from flask.ext.bcrypt import Bcrypt
+
+load_dotenv()
+
+host_db = os.getenv("host_db")
+user_db = os.getenv("user_db")
+password_db = os.getenv("password_db")
+schema_db = os.getenv("schema_db")
+
+db_conn_write = pymysql.connect(host_db, user_db, password_db,
+                                schema_db, cursorclass=pymysql.cursors.DictCursor)
+
+db_cursor = db_conn_write.cursor()
+
+from flask import Flask
+
+app = Flask(__name__)
+bcrypt = Bcrypt(app)
+
+@app.route('/users/register')
+def registerUser():
+    query_data = 'select * from users'
+    db_cursor.execute(query_data)
+    all_users = db_cursor.fetchall()
+    return dumps(all_users)
+
+app.run()
