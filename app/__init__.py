aefa4141ea030b49d04702774f8896e5f4b18772
SamsonChoo <hanye_choo@mymail.sutd.edu.sg>
2020-12-07 05:14:23 +0800

    Rearrange folders with boilerplate

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# local imports
+from config import app_config
+
+# db variable initialization
+db = SQLAlchemy()
+
+
+def create_app(config_name):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
+    db.init_app(app)
+
+    return app
