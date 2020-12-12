6a457f494681e5299a5800d8979a9b33a9d6471d
SamsonChoo <hanye_choo@mymail.sutd.edu.sg>
2020-12-12 21:53:48 +0800

    Setup basic flask environment

---
+++ config.py
@@ -0,0 +1,34 @@
+import os
+
+
+class Config(object):
+    """
+    Common configurations
+    """
+
+    # Put any configurations here that are common across all environments
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevelopmentConfig(Config):
+    """
+    Development configurations
+    """
+
+    DEBUG = True
+    SQLALCHEMY_ECHO = True
+
+
+class ProductionConfig(Config):
+    """
+    Production configurations
+    """
+
+    DEBUG = False
+
+
+app_config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig
+}
--- run.py
+++ run.py
@@ -1,8 +1,7 @@
 import os
-
 from app import create_app
 
-config_name = os.getenv('FLASK_CONFIG')
+config_name = os.environ.get('FLASK_ENV')
 app = create_app(config_name)
 
 
